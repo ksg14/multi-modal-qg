20f601ea973ca6ca392451490f6cdf67b1626c0e
Karanjit Gill <ksg207@live.com>
2021-05-24 19:12:32 +0530

    added greedy strategy

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 from torch.nn import Embedding, CrossEntropyLoss
+import torch.nn.functional as F
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
@@ -95,18 +96,27 @@ def validate (av_enc_model, text_enc_model, dec_model, dataloader, context_max_l
                 for di in range(pred_max_len):
                     dec_output, dec_hidden, dec_attention = dec_model (dec_input, context_len, av_enc_out, dec_hidden, all_enc_outputs)
                     # loss += criterion (dec_output, target [0][di].view (-1))
-                    
+
+                    # Greedy
+                    last_word_logits = dec_output   
+                    softmax_p = F.softmax(last_word_logits, dim=1).detach()
+                    word_index = torch.argmax (softmax_p, dim=1, keepdim=True)
+                    pred_words.append(dataloader.dataset.index_to_word [str (word_index [0] [0])])
+                    dec_input = word_index.detach ()
+                    print (f'nest word idx - {word_index [0] [0]} , next word - {pred_words [-1]}')
+
                     # Sampling
-                    # last_word_logits = y_pred[0][-1]
-                    # p = torch.nn.functional.softmax(last_word_logits, dim=0).detach().numpy()
-                    # word_index = np.random.choice(len(last_word_logits), p=p)
+                    # last_word_logits = dec_output[-1]
+                    # softmax_p = F.softmax(last_word_logits, dim=0).detach().numpy()
+                    # word_index = np.random.choice(len(last_word_logits), p=softmax_p)
                     # pred_words.append(dataloader.dataset.index_to_word [str (word_index)])
                     
                     # topk
-                    topv, topi = dec_output.data.topk(1)
-                    pred_words.append(dataloader.dataset.index_to_word [str (topi.item ())])
+                    # topv, topi = dec_output.data.topk(1)
+                    # pred_words.append(dataloader.dataset.index_to_word [str (topi.item ())])
 
-                    dec_input = topi.squeeze().detach().to (device)
+                    # dec_input = topi.squeeze().detach().to (device)
+                    # dec_input = word_index.detach().to (device)
 
                     if pred_words [-1] == '<end>':
                         del pred_words [-1]
@@ -114,6 +124,8 @@ def validate (av_enc_model, text_enc_model, dec_model, dataloader, context_max_l
                     # break
 
                 # val_loss += (loss.item () / target_len)
+                print (f'question - {question [0]}')
+                print (f'pred - {pred_words}')
                 question_str_list = question [0].split ()
                 val_bleu_1 += sentence_bleu (question_str_list, pred_words, weights=(1, 0, 0, 0))
                 val_bleu_2 += sentence_bleu (question_str_list, pred_words, weights=(0.5, 0.5, 0, 0))
@@ -248,22 +260,22 @@ if __name__ == '__main__':
     text_enc_model.to (device)
     dec_model.to (device)
 
-    epoch_stats = train (av_enc_model, text_enc_model, dec_model, train_dataloader, val_dataloader, text_enc_optimizer, dec_optimizer, criterion, config.epochs, device=device, context_max_len=config.context_max_lenth, pred_max_len=config.question_max_length)
+    # epoch_stats = train (av_enc_model, text_enc_model, dec_model, train_dataloader, val_dataloader, text_enc_optimizer, dec_optimizer, criterion, config.epochs, device=device, context_max_len=config.context_max_lenth, pred_max_len=config.question_max_length)
 
-    # validate (av_enc_model, text_enc_model, dec_model, val_dataloader, context_max_lenth, question_max_length, device)
+    validate (av_enc_model, text_enc_model, dec_model, val_dataloader, config.context_max_lenth, config.question_max_length, device)
 
-    try:
-        with open (config.stats_json_path, 'w') as f:
-            json.dump (epoch_stats, f)
-            print (f'Stats saved to {config.stats_json_path}')
-    except Exception:
-        pickle.dump(epoch_stats, open(config.stats_pkl_path, 'wb'))
-        print (f'Stats saved to {config.stats_pkl_path}')
+    # try:
+    #     with open (config.stats_json_path, 'w') as f:
+    #         json.dump (epoch_stats, f)
+    #         print (f'Stats saved to {config.stats_json_path}')
+    # except Exception:
+    #     pickle.dump(epoch_stats, open(config.stats_pkl_path, 'wb'))
+    #     print (f'Stats saved to {config.stats_pkl_path}')
     
-    try:
-        config.save_config ()
-    except Exception:
-        print (f'Unable to save config {str (Exception)}')
+    # try:
+    #     config.save_config ()
+    # except Exception:
+    #     print (f'Unable to save config {str (Exception)}')
     
     
     print ('Done !')
