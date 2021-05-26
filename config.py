47ef64923fc84fc623e051c3507f6822c6dfe05f
Karanjit Gill <ksg207@live.com>
2021-05-26 14:04:06 +0530

    testing save config

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ class Config():
     glove_matrix_file = glove_path / f'6B.{glove_emb_dim}_matrix.npy'
 
     # hyper-params
-    epochs = 100
+    epochs = 1
     lr = 1e-04
     optim='adam' # sgd, adam
     audio_emb = 128
