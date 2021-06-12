d7a1c3469ce5fa69d6c509b9b1de6639dfbd16c7
Karanjit Gill <ksg207@live.com>
2021-06-12 13:32:06 +0530

    added prophetnetlm model to download_models.py

--- download_models.py
+++ download_models.py
@@ -1,14 +1,14 @@
 import os
 import argparse
 
-from transformers import ProphetNetTokenizer, ProphetNetEncoder, ProphetNetForConditionalGeneration
+from transformers import ProphetNetTokenizer, ProphetNetEncoder, ProphetNetForConditionalGeneration, ProphetNetForCausalLM
 
 from config import Config
 
 def save_encoder (config: Config) -> int:
 	try:
 		tokenizer = ProphetNetTokenizer.from_pretrained('microsoft/prophetnet-large-uncased-squad-qg')
-		model = ProphetNetEncoder.from_pretrained('microsoft/prophetnet-large-uncased-squad-qg')
+		model = ProphetNetEncoder.from_pretrained('patrickvonplaten/prophetnet-large-uncased-standalone')
 	
 		tokenizer.save_pretrained(config.pretrained_tokenizer_path)
 		model.save_pretrained(config.pretrained_encoder_path)
@@ -20,6 +20,16 @@ def save_encoder (config: Config) -> int:
 def save_cg_decoder (config: Config) -> int:
 	try:
 		model = ProphetNetForConditionalGeneration.from_pretrained("microsoft/prophetnet-large-uncased-squad-qg", is_decoder = True, add_cross_attention=True)
+
+		model.save_pretrained(config.pretrained_decoder_path)
+	except Exception as e:
+		print (f'Error - {str (e)}')
+		return 1
+	return 0
+
+def save_lm_decoder (config: Config) -> int:
+	try:
+		model = ProphetNetForCausalLM.from_pretrained("microsoft/prophetnet-large-uncased-squad-qg", is_decoder = True, add_cross_attention=True)
 
 		model.save_pretrained(config.pretrained_decoder_path)
 	except Exception as e:
@@ -65,7 +75,6 @@ if __name__ == '__main__' :
 		if not os.path.exists (config.pretrained_decoder_path):
 			os.mkdir (config.pretrained_decoder_path)
 		
-		# save_electra_decoder (config)
-		save_cg_decoder (config)
+		save_lm_decoder (config)
 	
 	print ('Done!')
